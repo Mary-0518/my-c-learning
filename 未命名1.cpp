d2ace90aca978ff3145360f7de92e0a7f963e1a6
Mary-0518 <49830627+Mary-0518@users.noreply.github.com>
2019-04-21 18:30:21 +0800

    长方形的面积
    66666666666

---
+++ 未命名1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    int main(){
+    int x,y; 
+    printf("请输入长方形的宽和长，让我来帮你算算它的面积：");
+    scanf("%d %d",&x,&y);	
+    printf("长方形的面积是：%d",x*y);
+    	
+    return 0;	
+    }
+    
