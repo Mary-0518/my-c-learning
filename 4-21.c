174ffa7b2da46845368973e64037eb1155a29452
Mary-0518 <49830627+Mary-0518@users.noreply.github.com>
2019-04-21 15:47:18 +0800

    Create 4-21.c

---
+++ 4-21.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+int main(){
+	
+	printf("AER YOU ok??\nHallo!!\n");
+	printf("the resulr is:%d",23324+3223);	
+	
+	
+//printf(" 1+5 the result is:%d %d",1+5,4875968738+348587452974597243);
+
+
+	printf("3*4 is:%d",3*4);
+	return 0;	
+}
+
+
+//lesson two
+ 
+//变量
+//int n;//申明一个变量 
+//n=10;//=赋值符号 ==等于符号 
+//
+//int n=10;//初始化 
+
+//#include<stdio.h>
+//int main(){
+//	int a=10,b=20;
+//	printf("%d",a+b);
+//	return 0;	
+//} 
+
+//	区别： 
+//int n;
+//n=10;
+//
+//int n=10;
+
+//
+
+
+//常量 
+//1，2，3，23.324,34,3 
+
+
+
+//scanf()
+//printf()
+
+//#include<stdio.h>
+//int main(){
+//	int a,b;
+//	printf("please enter number:");
+//	scanf("%d %d",&a,&b);
+//	printf(" the sum:%d",a+b);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
