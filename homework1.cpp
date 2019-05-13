4ea68b1bc94cb3406a1711272336262facbbb0ae
Mary-0518 <49830627+Mary-0518@users.noreply.github.com>
2019-05-13 20:20:27 +0800

    判断奇偶数
    无简介 嚯哈哈哈

---
+++ homework1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("请输入一个整数:");
+	
+	scanf("%d",&a);
+	
+    if(a%2)
+    
+    printf("输入的是奇数");
+    
+    else
+    
+    printf("输入的是偶数");
+    return 0;
+	} 
+   
+	 
